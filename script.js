11849e8b0df852b709d4685d7aa15c6304ceb501
Arina231 <arinahonaa119@gmail.com>
2023-12-19 16:31:11 +0600

    modified:   script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
+ 
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
